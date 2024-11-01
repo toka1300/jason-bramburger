705394f89dcad15e8492636d5bff7855559eacad
toka1300 <caseytokarchuk@gmail.com>
2024-11-01 17:41:08 -0400

    publications templating implemented

---
+++ src/_data/research.js
@@ -0,0 +1,204 @@
+module.exports = {
+    "publications": [{
+            "authors": "J. Bramburger and G. Fantuzzi.",
+            "title": "Data-driven discovery of invariant measures.",
+            "reference": "Proceedings of the Royal Society A 480, (2024) 20230627",
+            "link": "https://royalsocietypublishing.org/doi/10.1098/rspa.2023.0627",
+            "repo": "https://github.com/jbramburger/data-measures"
+        },
+        {
+            "authors": "D. Hill, J. Bramburger, and D. Lloyd.",
+            "title": "Dihedral rings of patterns emerging from a Turing bifurcation.",
+            "link": "https://iopscience.iop.org/article/10.1088/1361-6544/ad2221",
+            "reference": "Nonlinearity 37, (2024) 035015.",
+            "repo": "https://github.com/Dan-Hill95/Dihedral-Rings"
+        },
+        {
+            "authors": "Z. Nicolaou and J. Bramburger.",
+            "title": "Complex localization mechanisms in networks of coupled oscillators: two case studies.",
+            "link": "https://pubs.aip.org/aip/cha/article-abstract/34/1/013131/3061429/Complex-localization-mechanisms-in-networks-of?redirectedFrom=fulltext",
+            "reference": "Chaos 34, (2024) 013131.",
+            "repo": "https://github.com/znicolaou/snakingoscillators"
+        },
+        {
+            "authors": "J. Bramburger and G. Fantuzzi.",
+            "title": "Auxiliary functions as Koopman observables: Data-driven analysis of dynamical systems via polynomial optimization.",
+            "link": "https://link.springer.com/article/10.1007/s00332-023-09990-2",
+            "reference": "Journal of Nonlinear Science 34, (2024) 8.",
+            "repo": "https://github.com/DCN-FAU-AvH/eDMD-sos",
+        },
+        {
+            "authors": "K. Kaheman, U. Fasel, J. Bramburger, B. Strom, J.N. Kutz, and S. Brunton.",
+            "title": "The experimental multi-arm pendulum on a cart: a benchmark system for chaos, learning, and control.",
+            "link": "https://www.sciencedirect.com/science/article/pii/S246806722300072X",
+            "reference": "HardwareX 15, (2023) e00465",
+            "repo": "https://github.com/dynamicslab/MultiArm-Pendulum",
+            "videoAbstract": "https://zenodo.org/record/6633719"
+        },
+        {
+            "authors": "J. Bramburger and M. Holzer.",
+            "title": "Pattern formation in random networks using graphons",
+            "link": "https://epubs.siam.org/doi/full/10.1137/21M1455875",
+            "reference": "SIAM Journal on Mathematical Analysis 55, (2023) 2150-2185."
+        },
+        {
+            "authors": "A. Chernyavsky, J. Bramburger, G. Fantuzzi, and D. Goluskin.",
+            "title": "Convex relaxations of integral variational problems: pointwise dual relaxation and sum-of-squares optimization",
+            "link": "https://epubs.siam.org/doi/full/10.1137/21M1455127",
+            "reference": "SIAM Journal on Optimization 33, (2023) 481-512."
+        },
+        {
+            "authors": "D. Hill, J. Bramburger, and D. Lloyd.",
+            "title": "Approximate localised dihedral patterns near a Turing instability.",
+            "link": "https://iopscience.iop.org/article/10.1088/1361-6544/acc508",
+            "reference": "Nonlinearity 36, (2023) 2567-2630.",
+            "repo": "https://github.com/Dan-Hill95/Localised-Dihedral-Patterns",
+            "coverArticle": "https://iopscience.iop.org/volume/0951-7715/37",
+            "dsWebFeature": "https://dsweb.siam.org/Media-Gallery/localised-patches"
+        },
+        {
+            "authors": "K. Kaheman, J. Bramburger, J.N. Kutz, and S. Brunton.",
+            "title": "Saddle transport and chaos in the double pendulum",
+            "link": "https://link.springer.com/article/10.1007/s11071-023-08232-0",
+            "reference": "Nonlinear Dynamics 111, (2023) 7199-7233.",
+            "repo": "https://github.com/dynamicslab/Saddle-Mediated-Transport-of-Double-Pendulum"
+        },
+        {
+            "authors": "M. Tian, J. Bramburger, and B. Sandstede.",
+            "title": "Snaking bifurcations of localized patterns on ring lattices.",
+            "link": "https://doi.org/10.1093/imamat/hxab023",
+            "reference": "IMA Journal of Applied Mathematics 86, (2021) 1112-1140."
+        },
+        {
+            "authors": "J. Bramburger, S. Brunton, and J.N. Kutz.",
+            "title": "Deep learning of conjugate mappings.",
+            "link": "https://www.sciencedirect.com/science/article/pii/S0167278921001652",
+            "reference": "Physica D 447, (2021) 133008.",
+            "repo": "https://github.com/jbramburger/Deep-Conjugacies",
+            "videoAbstract": "https://www.youtube.com/watch?v=SxRazghFL-Y"
+        },
+        {
+            "authors": "J. Bramburger and C. Henderson.",
+            "title": "The speed of traveling waves in a FKPP-Burgers system.",
+            "link": "https://link.springer.com/article/10.1007/s00205-021-01660-5",
+            "reference": "Archive for Rational Mechanics and Analysis 241, (2021) 643-681.",
+            "repo": "https://github.com/jbramburger/FKPP-Burgers"
+        },
+        {
+            "authors": "J. Bramburger.",
+            "title": "Isolas of multi-pulse solutions to lattice dynamical systems.",
+            "link": "https://doi.org/10.1017/prm.2020.44",
+            "reference": "Proceedings of the Royal Society of Edinburgh A 151, (2021) 916-952."
+        },
+        {
+            "authors": "J. Bramburger, J.N. Kutz, and S. Brunton.",
+            "title": "Data-driven stabilization of periodic orbits.",
+            "link": "https://ieeexplore.ieee.org/document/9378534",
+            "reference": "IEEE Access 9, (2021) 43504-43521.",
+            "repo": "https://github.com/jbramburger/Stabilizing_UPOs",
+            "videoAbstract": "https://www.youtube.com/watch?v=72MeRaNki8E&feature=youtu.be"
+        },
+        {
+            "authors": "J. Bramburger.",
+            "title": "Exact minimum speed of traveling waves in a Keller-Segel model.",
+            "link": "https://doi.org/10.1016/j.aml.2020.106594",
+            "reference": "Applied Mathematics Letters 111, (2021) 106594."
+        },
+        {
+            "authors": "J. Bramburger and D. Goluskin.",
+            "title": "Minimum wave speeds in monostable reaction-diffusion equations: sharp bounds by polynomial optimization.",
+            "link": "https://royalsocietypublishing.org/doi/10.1098/rspa.2020.0450",
+            "reference": "Proceedings of the Royal Society A 476, (2020) 20200450.",
+            "repo": "https://github.com/jbramburger/Minimum-Wave-Speeds",
+            "videoAbstract": "https://www.youtube.com/watch?v=LtjwMMpxLt8"
+        },
+        {
+            "authors": "J. Bramburger, D. Dylewsky, and J.N. Kutz.",
+            "title": "Sparse identification of slow timescale dynamics.",
+            "link": "https://journals.aps.org/pre/abstract/10.1103/PhysRevE.102.022204",
+            "reference": "Physical Review E 102, (2020) 022204.",
+            "repo": "https://github.com/jbramburger/Slow-Discovery",
+            "videoAbstract": "https://www.youtube.com/watch?v=2ji2-XUgVl0"
+        },
+        {
+            "authors": "J. Bramburger and B. Sandstede.",
+            "title": "Localized patterns in planar bistable weakly coupled lattice systems.",
+            "link": "https://iopscience.iop.org/article/10.1088/1361-6544/ab7d1e",
+            "reference": "Nonlinearity 33, (2020) 3500-3525."
+        },
+        {
+            "authors": "J. Bramburger and B. Sandstede.",
+            "title": "Spatially localized structures in lattice dynamical systems.",
+            "link": "https://link.springer.com/article/10.1007/s00332-019-09584-x",
+            "reference": "Journal of Nonlinear Science 30, (2020) 603-644."
+        },
+        {
+            "authors": "J. Bramburger and J.N. Kutz.",
+            "title": "Poincaré maps for multiscale physics discovery and nonlinear Floquet theory.",
+            "link": "https://www.sciencedirect.com/science/article/pii/S0167278919305470?via%3Dihub",
+            "reference": "Physica D 408, (2020) 132497.",
+            "repo": "https://github.com/jbramburger/Poincare-Maps",
+            "videoAbstract": "https://www.youtube.com/watch?v=2Z2oYc5IS7M"
+        },
+        {
+            "authors": "J. Bramburger, J. Cuevas-Maraver, and P.G. Kevrekidis.",
+            "title": "Vortex pairs in the discrete nonlinear Schrodinger equation.",
+            "link": "https://iopscience.iop.org/article/10.1088/1361-6544/ab6f4c",
+            "reference": "Nonlinearity 33, (2020) 2159-2180."
+        },
+        {
+            "authors": "J. Bramburger.",
+            "title": "Ultracontractive properties for directed graph semigroups with applications to coupled oscillators.",
+            "link": "https://doi.org/10.4153/S0008439519000390",
+            "reference": "Canadian Mathematical Bulletin 63, (2020) 13-30."
+        },
+        {
+            "authors": "J. Bramburger.",
+            "title": "Stable periodic solutions to lambda-omega lattice dynamical systems.",
+            "link": "https://www.sciencedirect.com/science/article/pii/S0022039619304607",
+            "reference": "Journal of Differential Equations 268, (2020) 3201-3254."
+        },
+        {
+            "authors": "S. Alexander, J. Bramburger, and E. McDonough.",
+            "title": "Dark disk substructure and superfluid dark matter.",
+            "link": "https://www.sciencedirect.com/science/article/pii/S0370269319305854?via%3Dihub",
+            "reference": "Physics Letters B 797, (2019) 134871."
+        },
+        {
+            "authors": "J. Bramburger, D. Altschuler, C. Avery, T. Sangsawang, M. Beck, P. Carter, and B. Sandstede.",
+            "title": "Localized radial roll patterns in higher space dimensions.",
+            "link": "https://epubs.siam.org/doi/pdf/10.1137/18M1218728",
+            "reference": "SIAM Journal on Applied Dynamical Systems 18, (2019) 1420–1453.",
+            "repo": "https://github.com/sandstede-lab/Radial-Roll-Patterns"
+        },
+        {
+            "authors": "",
+            "title": "",
+            "link": "",
+            "reference": "",
+            "repo": ""
+        },
+        {
+            "authors": "",
+            "title": "",
+            "link": "",
+            "reference": "",
+            "repo": ""
+        },
+        {
+            "authors": "",
+            "title": "",
+            "link": "",
+            "reference": "",
+            "repo": ""
+        },
+        {
+            "authors": "",
+            "title": "",
+            "link": "",
+            "reference": "",
+            "repo": ""
+        },
+
+    ]
+};
