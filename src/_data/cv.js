e35ee89a1db804d03899c68de4e7c567d3e1167f
toka1300 <caseytokarchuk@gmail.com>
2024-11-08 15:52:17 -0500

    fixed image title

---
+++ src/_data/cv.js
@@ -0,0 +1,134 @@
+module.exports = {
+    "employment": [
+        {
+            "title": "Assistant Professor",
+            "department": "Department of Mathematics and Statistics, Concordia University",
+            "location": "Montreal, QC, Canada",
+            "dates": "since July 2022"
+        },
+        {
+            "title": "Assistant Professor",
+            "department": "Department of Mathematical Sciences, George Mason University",
+            "location": "Fairfax, VA, United States of America",
+            "dates": "August 2021 - May 2022"
+        },
+        {
+            "title": "Acting Instructor",
+            "department": "Department of Applied Mathematics, University of Washington",
+            "location": "Seattle, WA, United States of America",
+            "dates": "September 2020 - August 2021"
+        },
+        {
+            "title": "PIMS Postdoctoral Fellow",
+            "department": "Department of Mathematics and Statistics, University of Victoria",
+            "location": "Victoria, BC, Canada",
+            "dates": "September 2019 - August 2020"
+        },
+        {
+            "title": "NSERC Postdoctoral Fellow",
+            "department": "Division of Applied Mathematics, Brown University",
+            "location": "Providence, RI, United States of America",
+            "dates": "September 2017 - August 2019"
+        },
+        {
+            "title": "Instructor",
+            "department": "Academic Access Centre, Algonquin College",
+            "location": "Ottawa, ON, Canada",
+            "dates": "May 2017 - August 2017"
+        },
+        {
+            "title": "Teaching Assistant",
+            "department": "Department of Mathematics and Statistics, University of Ottawa",
+            "location": "Ottawa, ON, Canada",
+            "dates": "September 2011 - August 2017"
+        },
+        {
+            "title": "Lecturer",
+            "department": "Department of Mathematics and Statistics, University of Ottawa",
+            "location": "Ottawa, ON, Canada",
+            "dates": "September 2016 - December 2016"
+        },
+        {
+            "title": "Statistical Analyst",
+            "department": "Academic Operations and Planning Department, Algonquin College",
+            "location": "Ottawa, ON, Canada",
+            "dates": "May 2011 - August 2011, May 2012 - August 2012"
+        },
+        {
+            "title": "Teaching Assistant",
+            "department": "Department of Mathematics and Computer Science, Nipissing University",
+            "location": "North Bay, ON, Canada",
+            "dates": "September 2010 - May 2011"
+        },
+        {
+            "title": "Math Drop-In Centre Attendant",
+            "department": "Department of Mathematics and Computer Science, Nipissing University",
+            "location": "North Bay, ON, Canada",
+            "dates": "September 2008 - May 2011"
+        }
+    ],
+    "awards": [
+        {
+            "name": "Fellow of the Institute of Advanced Studies",
+            "source": "University of Surrey",
+            "dates": "July 2021 - June 2022",
+            "link": "https://www.ias.surrey.ac.uk/"
+        },
+        {
+            "name": "PIMS Postdoctoral Fellowship",
+            "source": "Pacific Institute for the Mathematical Sciences",
+            "dates": "September 2019 - August 2020",
+            "link": "https://www.pims.math.ca/scientific/postdoctoral"
+        },
+        {
+            "name": "AMS-Simons Travel Grant",
+            "source": "American Mathematical Society and the Simons Foundation",
+            "dates": "July 2018 - August 2019",
+            "link": "http://www.ams.org/programs/travel-grants/AMS-SimonsTG"
+        },
+        {
+            "name": "NSERC Postdoctoral Fellowship",
+            "source": "Natural Sciences and Engineering Research Council",
+            "dates": "September 2017 - August 2019",
+            "link": "http://www.nserc-crsng.gc.ca/Students-Etudiants/PD-NP/PDF-BP_eng.asp"
+        },
+        {
+            "name": "Dean's Scholarship",
+            "source": "University of Ottawa",
+            "dates": "November 2017",
+        },
+        {
+            "name": "Ontario Graduate Scholarship",
+            "source": "Ontario Ministry of Training, Colleges and Universities",
+            "dates": "September 2013 - August 2017",
+            "link": "https://www.osap.gov.on.ca/OSAPPortal/en/A-ZListofAid/PRDR019245.html"
+        },
+        {
+            "name": "Excellence Scholarship",
+            "source": "University of Ottawa",
+            "dates": "September 2013 - August 2017",
+            "link": "https://www.uottawa.ca/graduate-studies/students/awards/excellence-scholarships"
+        },
+        {
+            "name": "Teaching Assistant of the Year",
+            "source": "Faculty of Science, University of Ottawa",
+            "dates": "November 2016",
+        },
+        {
+            "name": "André Dabrowski Scholarship",
+            "source": "Department of Mathematics and Statistics, University of Ottawa",
+            "dates": "June 2016",
+            "link": "https://scholarships.uottawa.ca/p/a/17425/"
+        },
+        {
+            "name": "Dean's Scholarship",
+            "source": "University of Ottawa",
+            "dates": "November 2013",
+        },
+        {
+            "name": "Carl Sanders Scholarship",
+            "source": "Nipissing University",
+            "dates": "September 2007 - May 2011",
+        }
+    ]    
+}
