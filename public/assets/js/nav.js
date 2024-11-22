d5fb1c9ba71ae6022f91dd427ae687c49b949c42
toka1300 <caseytokarchuk@gmail.com>
2024-11-21 20:25:33 -0500

    update font reference

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,1 +1,37 @@
-(()=>{var o=document.querySelector("body"),s=document.querySelector("#cs-navigation"),c=document.querySelector("#cs-navigation .cs-toggle");c.addEventListener("click",function(){c.classList.toggle("cs-active"),s.classList.toggle("cs-active"),o.classList.toggle("cs-open"),n()});function n(){let e=document.querySelector("#cs-expanded");e.getAttribute("aria-expanded")==="false"?e.setAttribute("aria-expanded","true"):e.setAttribute("aria-expanded","false")}document.addEventListener("scroll",e=>{document.documentElement.scrollTop>=100?document.querySelector("body").classList.add("scroll"):document.querySelector("body").classList.remove("scroll")});var r=Array.from(document.querySelectorAll("#cs-navigation .cs-dropdown"));for(let e of r){let t=()=>{e.classList.toggle("cs-active")};e.addEventListener("click",t)}})();
+(() => {
+  // src/assets/js/nav.js
+  var CSbody = document.querySelector("body");
+  var CSnavbarMenu = document.querySelector("#cs-navigation");
+  var CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+  CShamburgerMenu.addEventListener("click", function() {
+    CShamburgerMenu.classList.toggle("cs-active");
+    CSnavbarMenu.classList.toggle("cs-active");
+    CSbody.classList.toggle("cs-open");
+    ariaExpanded();
+  });
+  function ariaExpanded() {
+    const csUL = document.querySelector("#cs-expanded");
+    const csExpanded = csUL.getAttribute("aria-expanded");
+    if (csExpanded === "false") {
+      csUL.setAttribute("aria-expanded", "true");
+    } else {
+      csUL.setAttribute("aria-expanded", "false");
+    }
+  }
+  document.addEventListener("scroll", (e) => {
+    const scroll = document.documentElement.scrollTop;
+    if (scroll >= 100) {
+      document.querySelector("body").classList.add("scroll");
+    } else {
+      document.querySelector("body").classList.remove("scroll");
+    }
+  });
+  var dropDowns = Array.from(document.querySelectorAll("#cs-navigation .cs-dropdown"));
+  for (const item of dropDowns) {
+    const onClick = () => {
+      item.classList.toggle("cs-active");
+    };
+    item.addEventListener("click", onClick);
+  }
+})();
+//# sourceMappingURL=nav.js.map
